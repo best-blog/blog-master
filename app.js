f308676511e5bf05de17c6d959db540acc663950
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-09-10 19:45:12 -0300

    criado rota inicial para usuario

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const usersRouters = require('./routes/blog-users')
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api', (req, res) => res.json({ok: 'status ok'}))
+app.use('/api/users', usersRouters)
 
 app.listen(5000, () => console.log('server rodando na porta 5000'))
