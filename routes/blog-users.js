abe467e4b0ea4081526be38dead3073c6fa1c8a1
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-09-17 19:56:21 -0300

    criado controller usuario

--- routes/blog-users.js
+++ routes/blog-users.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
+const usersController = require('../controllers/users-controllers')
+
 router.get('/', (req, res) => {
     res.json({users: 'users ok'});
 })
 
-router.get('/123', (req, res) => {
-    res.json({users: 'users 123'});
-})
+router.get('/123', usersController.testUsers)
 
 module.exports = router;
